8118ebd43256cf254af22b5eb6794119a61422b3
Scott McPeak <scott.g.mcpeak@gmail.com>
2021-06-11 18:37:31 -0700

    example-test.cc: Add tests for MVisitor.
    example.ast: Fix bug: "custom mtraverse" was wrong.
    
    I guess, originally, I didn't have support for traversing FakeList,
    so I thought the custom action was needed.  But it does not check
    for the list end, so would always crash; and anyway is not needed
    since I do traverse FakeLists.

--- example-test.cc
+++ example-test.cc
@@ -10,8 +10,10 @@
 using namespace std;
 
 
-void testNode()
+static void testNode()
 {
+  cout << "---- testNode ----\n";
+
   Node *n1 = new Node(1,2);
   xassert(n1->x == 1);
   xassert(n1->y == 2);
@@ -21,8 +23,10 @@ void testNode()
 }
 
 
-void testNodeList()
+static void testNodeList()
 {
+  cout << "---- testNodeList ----\n";
+
   FakeList<Node> *list = FakeList<Node>::emptyList();
   list = fl_prepend(list, new Node(7,8));
   list = fl_prepend(list, new Node(4,5));
@@ -35,10 +39,88 @@ void testNodeList()
 }
 
 
+class TestMVisitor : public ExampleMVisitor {
+public:
+  bool visitSuper(Super *&obj) override;
+  bool visitNode(Node *&node) override;
+};
+
+bool TestMVisitor::visitSuper(Super *&obj)
+{
+  cout << "visiting Super: x=" << obj->x << '\n';
+
+  ASTSWITCH(Super, obj) {
+    ASTCASE(Sub3, s3) {
+      // Replace the 's1' field.
+      delete s3->s1;
+      s3->s1 = new Sub2(5 /*x*/, 3 /*z*/);
+    }
+
+    ASTNEXT(Sub2, s2) {
+      if (s2->z == 9) {
+        // Replace this object.
+        delete obj;
+        obj = new Sub2(6 /*x*/, 4 /*z*/);
+      }
+    }
+
+    ASTENDCASED
+  }
+  return true;
+}
+
+
+bool TestMVisitor::visitNode(Node *&node)
+{
+  cout << "visiting Node: x=" << node->x << " y=" << node->y << '\n';
+
+  Node *newNode = new Node(node->x + 1, node->y + 1);
+  newNode->next = node->next;
+
+  delete node;
+  node = newNode;
+
+  return true;
+}
+
+
+static void testMVisitor()
+{
+  cout << "---- testMVisitor ----\n";
+  TestMVisitor mvisitor;
+
+  {
+    Super *tree = new Sub3(1 /*x*/,
+      new Sub1(7 /*x*/, 2 /*y*/),
+      new Sub2(8 /*x*/, 9 /*z*/));
+    tree->debugPrint(cout, 0, "before");
+
+    mvisitor.mtraverse(tree);
+    tree->debugPrint(cout, 0, "after");
+
+    delete tree;
+  }
+
+  {
+    FakeList<Node> *list = FakeList<Node>::emptyList();
+    list = fl_prepend(list, new Node(3,4));
+    list = fl_prepend(list, new Node(1,2));
+    NodeList *nodeList = new NodeList(list);
+    nodeList->debugPrint(cout, 0, "before");
+
+    mvisitor.mtraverse(nodeList);
+    nodeList->debugPrint(cout, 0, "after");
+
+    delete nodeList;
+  }
+}
+
+
 int main()
 {
   testNode();
   testNodeList();
+  testMVisitor();
 
   cout << "example-test passed\n";
   return 0;
